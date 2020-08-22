dd187c59dfeb9d06ee10a7052fdf251271c62dab
Hamid Molareza <kitm.hamid@gmail.com>
2020-08-23 01:50:17 +0430

    Change fileType structure.

--- FileSignatureValidation/FileType.cs
+++ FileSignatureValidation/FileType.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections.Generic;
-
-namespace FileSignatureValidation
-{
-    public class FileType
-    {
-        public string TypeName { get; set; }
-        public List<byte[]> Signatures { get; set; }
+﻿namespace FileSignatureValidation {
+    public class FileType {
+        public string TypeName { get; set; } = null!;
+        public byte[] Signature { get; set; } = null!;
     }
 }
